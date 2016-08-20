49ff0bb532f131e3d6f7854f9a22caf788e6106b
rishi <me.rishiverma@gmail.com>
2016-08-20 21:49:01 +0530

    anagramsSearch

---
+++ anagramsSearch.py
@@ -0,0 +1,26 @@
+final=[]
+def permutations(string, step = 0):
+
+    # if we've gotten to the end, print the permutation
+    if step == len(string):
+        final.append("".join(string))
+
+    # everything to the right of step has not been swapped yet
+    for i in range(step, len(string)):
+
+        # copy the string (store as array)
+        string_copy = [character for character in string]
+
+        # swap the current index with the step
+        string_copy[step], string_copy[i] = string_copy[i], string_copy[step]
+
+        # recurse on the portion of the string that has not been swapped yet (now it's index will begin with step + 1)
+        permutations(string_copy, step + 1)
+a='forxxorfxdofr'
+b='for'
+permutations(b)
+count=0
+for i in final:
+    if i in a:
+        count+=1
+print count
